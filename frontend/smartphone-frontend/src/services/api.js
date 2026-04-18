8dcbc90c7b04f74a15664d853ae892eafb03f2fc
aminhmdov000 <115492102+aminhmdov000@users.noreply.github.com>
2026-04-18 20:22:37 +0300

    Change API base URL to production
    Updated the base URL for the API to the production endpoint.

--- frontend/smartphone-frontend/src/services/api.js
+++ frontend/smartphone-frontend/src/services/api.js
@@ -1,55 +1,6 @@
-// import axios from "axios";
-// const api = axios.create({
-//   baseURL: 'http://localhost:3000/api'
-// });
-// //request interceptor (add token)
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('token');
-//   console.log('Token:', token)
-//   if(token){
-//     config.headers['Authorization'] = `Bearer ${token}`
-//   }
-//   console.log('Headers:', config.headers)
-//   return config;
-// },
-//   (error) => Promise.reject(error)
-// );
-// //response interceptor (refresh token logic)
-// api.interceptors.response.use((response) => response, async(error) => {
-//   const originalRequest = error.config;
-//   //pretend infinite loop
-//   if(error.response?.status === 401 && !originalRequest._retry){
-//     originalRequest._retry = true
-//     const refreshToken = localStorage.getItem('refreshToken');
-//     if(!refreshToken){
-//       localStorage.clear();
-//       window.location.href = '/login';
-//       return Promise.reject(error);
-//     }
-//     try {
-//       const res = await api.post('/auth/refresh-token', {refreshToken})
-//       // const newAccessToken = res.data.accessToken;
-//       const newAccessToken = res.data.token;
-//       const refreshToken = res.data.refreshToken;
-//       localStorage.setItem('token', newAccessToken);
-//       localStorage.setItem('refreshToken', refreshToken)
-//       originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-//       return api(originalRequest);
-//     } catch (err) {
-//       //auto logout if failed 
-//       localStorage.removeItem('user');
-//       localStorage.removeItem('token');
-//       localStorage.removeItem('refreshToken');
-//       window.location.href = '/login';
-//       return Promise.reject(err)
-//     }
-//   }
-//   return Promise.reject(error)
-// });
-// export default api;
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const baseURL = import.meta.env.VITE_API_URL || 'https://smartphone-store-backend-9k4t.onrender.com/api';
 
 const api = axios.create({ baseURL });
 
