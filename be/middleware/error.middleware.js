309445dd852e120311d68c544cdb34ab279137fd
aminhmdov000 <115492102+aminhmdov000@users.noreply.github.com>
2026-04-20 15:42:55 +0300

    Update error.middleware.js

--- be/middleware/error.middleware.js
+++ be/middleware/error.middleware.js
@@ -1,13 +1,3 @@
-// const logger = require('../utils/logger');
-// exports.errorHandler = (err,req,res,next) => {
-//   logger.error({
-//     message: err.message,
-//     stack: err.stack,
-//     url: req.originalUrl,
-//     method: req.method
-//   });
-//   res.status(err.status || 500).json({error: err.message})
-// };
 exports.errorHandler = (err, req, res, next) => {
     console.error("XƏTA BAŞ VERDİ:", err); 
     res.status(500).json({ success: false, message: err.message });
