86c9eb0f598513d5cf96b50cdf8c3eb51e4bbb07
aminhmdov000 <115492102+aminhmdov000@users.noreply.github.com>
2026-04-20 17:31:13 +0300

    Update db.js

--- be/config/db.js
+++ be/config/db.js
@@ -1,31 +1,13 @@
-// const mysql = require('mysql2/promise');
-// require('dotenv').config(); 
-
-// const connection = mysql.createPool(process.env.DATABASE_URL);
-
-// connection.connect(err => {
-//     if (err) {
-//         console.log("Error:", err.message);
-//     } else {
-//         console.log("It was connected to Railway via .env! ✅");
-//     }
-// });
-
-// module.exports = connection;
-
 const mysql = require('mysql2/promise');
-
-// Pool istifadə etmək canlı layihələrdə daha sürətli və stabil işləyir
 const pool = mysql.createPool(process.env.DATABASE_URL);
 
-// Bağlantını yoxlamaq üçün kiçik bir test (ixtiyari)
 (async () => {
     try {
         const connection = await pool.getConnection();
         console.log("Railway successfully joined the database with Promise!");
-        connection.release(); // Bağlantını hovuza geri qaytarırıq
+        connection.release();
     } catch (err) {
-        console.error("Baza bağlantısında xəta:", err.message);
+        console.error("Error:", err.message);
     }
 })();
 
