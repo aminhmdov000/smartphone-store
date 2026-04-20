93df0a2270195047e8f939c4a944c1d74f7a75cb
aminhmdov000 <115492102+aminhmdov000@users.noreply.github.com>
2026-04-20 17:15:59 +0300

    Update app.js

--- be/app.js
+++ be/app.js
@@ -1,40 +1,27 @@
-const helmet = require('helmet'); // Security headers middleware
+const helmet = require('helmet'); 
 const path = require('path')
-//IMPORT EXPRESS FRAMEWORK
 const express = require('express');
-//ALLOW FRONTEND TO CONNECT TO BACKEND (CROSS-ORIGIN RESOURCE SHARING)
 const cors = require('cors');
-//IMPORT PRODUCT ROUTTES FILE
 const {limiter} = require('./middleware/rateLimiter');
 const productRoutes = require('./routes/productRoutes');
 const {errorHandler} = require('./middleware/error.middleware');
-//IMPORT AUTH ROUTES FILE
 const authRoutes = require('./routes/auth.routes');
-//IMPORT CART ROUTES FILE
 const cartRoutes = require('./routes/cart.routes');
-//IMPORT ORDER ROUTES
 const orderRoutes = require('./routes/order.routes');
 const userRoutes = require('./routes/user.routes');
 const contactRoutes = require('./routes/contactus.routes');
-//CREATE EXPRESS APPLICATION
 const app = express();
+
 app.set('trust proxy', 1);
-//MIDDLEWARE TO PARSE JSON BODIES
 app.use(express.json());
-//MIDDLEWARE TO ENABLE CORS FOR ALL ROUTES (CALLING FRONTEND FROM DIFFERENT ORIGIN)
 app.use(cors());
 app.use(helmet({
   crossOriginResourcePolicy: { policy: 'cross-origin' }
 }));
-
-// app.use(limiter);
-//USE PRODUCT ROUTES FOR ANY REQUEST STARTING WITH /api/products
+app.use(limiter);
 app.use('/api/products', productRoutes);
-//USE AUTH ROUTES FOR ANY REQUEST STARTING WITH /api/auth
 app.use('/api/auth', authRoutes);
-//USE CART ROUTES FOR ANY REQUEST STARTING WITH /api/cart
 app.use('/api/cart', cartRoutes);
-//USE ORDER ROUTES FOR ANY REQUEST STARTING WITH /api/orders
 app.use('/api/orders', orderRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/contact', contactRoutes)
