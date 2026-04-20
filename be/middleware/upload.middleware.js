5a4ca6e382357e42b3de9ca52cd4171ca5719e8e
aminhmdov000 <115492102+aminhmdov000@users.noreply.github.com>
2026-04-20 14:50:06 +0300

    Update upload.middleware.js

--- be/middleware/upload.middleware.js
+++ be/middleware/upload.middleware.js
@@ -1,12 +1,12 @@
-const multer = require('multer'); // upload library
-const path = require('path'); //for file extension
-const storage = multer.diskStorage({ // save to hard disk(uploads folder)
-  destination: (req,file,cb) => { // request, uploaded file, callback
-    cb(null, path.join(__dirname, '..', 'uploads')); // save files to backend uploads folder
+const multer = require('multer'); 
+const path = require('path'); 
+const storage = multer.diskStorage({ 
+  destination: (req,file,cb) => { 
+    cb(null, path.join(__dirname, '..', 'uploads')); 
   },
   filename: (req,file,cb) => { 
     const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1e9) 
-    + path.extname(file.originalname); // 17000000000-123456789.png
+    + path.extname(file.originalname); 
     cb(null,uniqueName);
   }
 });
